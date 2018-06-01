c5ad2d4c8648c4f6bbabb2ff735c9944d4bde2eb
bruce.jcw <bruce.jcw@gmail.com>
2018-06-01 10:58:02 +0800

    replace to awesome-fastclick

--- pkg/settings/vendor.js
+++ pkg/settings/vendor.js
@@ -2,9 +2,9 @@ module.exports = {
   dependencies: {
     'es6-promise': '^4.1.1',
     'awesome-fastclick': '^1.0.6',
-    'vue': '^2.5.8',
+    vue: '^2.5.8',
     'vue-router': '^3.0.1',
-    'vuex': '^3.0.1',
+    vuex: '^3.0.1',
     'whatwg-fetch': '^2.0.3',
   },
 }
